94bf82447ad81db1bcec444e1488c6fa3a19ef1f
Abdullah Shaikh <abdullahshaikhneo@gmail.com>
2023-05-22 21:48:35 +0530

    image test two

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/test.jpg">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <img src="test.jpg" class="resizable-image" />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -22,5 +23,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.resizable-image {
+  width: 200px; /* Adjust the width as per your requirement */
+  height: auto; /* Automatically adjust the height to maintain the aspect ratio */
 }
 </style>
